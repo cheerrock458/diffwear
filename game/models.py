e6168af29ff2f4cb107ad880be6df0eef4bf91fd
cheerrock458 <gumpun888@gmail.com>
2026-04-20 02:04:58 +0700

    Add files via upload

---
+++ game/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# 🔥 ห้องแชท
+class Conversation(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat2')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user1} - {self.user2}"
+
+
+# 💬 ข้อความ
+class ChatMessage(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+   
