65c1534f95b0d4192e8d9a21abafb6de9b909571
cheerrock458 <gumpun888@gmail.com>
2026-04-20 02:48:01 +0700

    ignore staticfiles and media

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1q!zmi*u=8#4xh-ki1+v^mf6ydduqopkk%scue@ug6^w_7%0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
