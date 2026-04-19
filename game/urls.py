73a12df6f44461c2d2393cb97e2dddf712815379
cheerrock458 <gumpun888@gmail.com>
2026-04-20 02:16:05 +0700

    first commit

---
+++ game/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('add/', views.add_product, name='add_product'),
+    path('delete/<int:id>/', views.delete_product, name='delete_product'),
+
+    path('cart/', views.cart, name='cart'),
+    path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('chat/', views.chat_list, name='chat_list'),
+    path('chat/<int:id>/', views.chat_detail, name='chat_detail'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('seller-inbox/', views.seller_inbox, name='seller_inbox'),
+    path('buy-now/<int:id>/', views.buy_now, name='buy_now'),   
+]
